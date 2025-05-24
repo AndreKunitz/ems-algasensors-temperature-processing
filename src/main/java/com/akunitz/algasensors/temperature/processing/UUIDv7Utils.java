980a7c6cf9abc314633e7ec2373b3b020439773d
Andre KunitzGuilherme Kunitz <akunitz@paypal.com>
2025-05-24 20:44:23 -0300

    Usando o UUID v4 e v7 com Java

---
+++ src/main/java/com/akunitz/algasensors/temperature/processing/UUIDv7Utils.java
@@ -0,0 +1,23 @@
+package com.akunitz.algasensors.temperature.processing;
+
+import java.time.Instant;
+import java.time.OffsetDateTime;
+import java.time.ZoneId;
+import java.util.UUID;
+
+public class UUIDv7Utils {
+
+    public UUIDv7Utils() {
+        // Private constructor to prevent instantiation
+    }
+
+    public static OffsetDateTime extractOffsetDateTime(UUID uuid) {
+        if (uuid == null) {
+            return null;
+        }
+
+        long timestamp = uuid.getMostSignificantBits() >>> 16; // Extract the timestamp part
+
+        return OffsetDateTime.ofInstant(Instant.ofEpochMilli(timestamp), ZoneId.systemDefault());
+    }
+}
---
+++ src/test/java/com/akunitz/algasensors/temperature/processing/UUIDv7Test.java
@@ -0,0 +1,22 @@
+package com.akunitz.algasensors.temperature.processing;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.OffsetDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class UUIDv7Test {
+
+    @Test
+    void shouldGenerateUUIDv7() {
+        UUID uuid = IdGenerator.generateTimeBasedUUID();
+
+        OffsetDateTime uuidDateTime = UUIDv7Utils.extractOffsetDateTime(uuid).truncatedTo(ChronoUnit.MINUTES);
+        OffsetDateTime currentDateTime = OffsetDateTime.now().truncatedTo(ChronoUnit.MINUTES);
+
+        assertThat(uuidDateTime).isEqualTo(currentDateTime);
+    }
+}
